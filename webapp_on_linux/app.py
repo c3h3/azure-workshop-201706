53af0eed52ed0d5ae383fc3a820dfc1b63fc6505
Chia-Chi Chang <c3h3.tw@gmail.com>
2017-06-22 02:58:51 +0800

    LAB6 basic sample

---
+++ webapp_on_linux/app.py
@@ -0,0 +1,25 @@
+import os
+APP_ID  = os.environ.get("APP_ID", "AzureWorkshop201706")
+HOST  = os.environ.get("HOST", "0.0.0.0")
+PORT  = os.environ.get("PORT", 5000)
+DEBUG  = [True, False][int(os.environ.get("DEBUG", 0))]
+
+try:
+    from local_settings import *
+except:
+    pass
+
+from flask import Flask, request, abort, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("home.html",
+        title = 'Home',
+        app_id = APP_ID)
+
+
+if __name__ == "__main__":
+    app.debug = DEBUG
+    app.run(host=HOST, port=PORT)
